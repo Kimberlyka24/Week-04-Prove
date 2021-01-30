01cdd34607e7759b6acb05df5c6be770a8ed1ac0
Zabrodin <mailfornikolay@gmail.com>
2021-01-30 14:12:35 +0300

    Hi form Nikolay

--- src/provefour/Main.java
+++ src/provefour/Main.java
@@ -3,6 +3,7 @@ package provefour;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Kim.");
+        System.out.println("Hello, my name is Kim.");
+        System.out.println("Hello Kim, my name is Nikolay!");
     }
 }
